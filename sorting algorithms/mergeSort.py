e7e3079892cf3e62e7d2cb8bceb5a08c1e70e405
Pushkar Desai <pushkar.desai999@hotmail.com>
2021-02-11 11:52:26 +0530

    Merge sort

---
+++ sorting algorithms/mergeSort.py
@@ -0,0 +1,22 @@
+def mergeSort(unsortedList):
+    if len(unsortedList) == 1 : return unsortedList
+
+    firstHalf = unsortedList[:len(unsortedList)//2]
+    secondHalf = unsortedList[len(unsortedList)//2:]
+
+    firstHalf = mergeSort(firstHalf)
+    secondHalf = mergeSort(secondHalf)
+    
+    i = j = 0
+    output = []
+    while(i < len(firstHalf) and j < len(secondHalf)):
+        if(firstHalf[i] < secondHalf[j]):
+            output.append(firstHalf[i])
+            i += 1
+        else:
+            output.append(secondHalf[j])
+            j += 1
+    return output + firstHalf[i:len(firstHalf)] + secondHalf[j:len(secondHalf)]
+
+if __name__ == "__main__":
+    print(f'mergeSort : {mergeSort([5, 3, 9, 1, 4])}')
